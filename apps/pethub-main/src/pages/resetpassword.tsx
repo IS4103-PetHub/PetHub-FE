718a592cb46180c96bd505833022bf2b8ce4e2cb
Ryo <ryo200013@gmail.com>
2023-09-06 15:44:01 +0800

    PR comments for forgot/reset PW

--- apps/pethub-main/src/pages/resetpassword.tsx
+++ apps/pethub-main/src/pages/resetpassword.tsx
@@ -28,37 +28,29 @@ export default function Login() {
     },
     validate: {
       password: validatePassword,
-      confirmPassword: validatePassword,
+      confirmPassword: (value, values) =>
+        value !== values.password ? "Passwords did not match" : null,
     },
   });
 
   const handleResetPassword = async () => {
-    if (form.values.password !== form.values.confirmPassword) {
+    // Get the token out of the URL and attempt API call
+    console.log("The reset password token is", router.query.token);
+    const resetPasswordPayload: ResetPasswordPayload = {
+      token: parseRouterQueryParam(router.query.token),
+      newPassword: form.values.password,
+    };
+    try {
+      await resetPasswordService(resetPasswordPayload);
+      setIsResetSuccessful(true);
+    } catch (e: AxiosError | any) {
       notifications.show({
-        message: "Passwords do not match",
+        message:
+          (e.response && e.response.data && e.response.data.message) ||
+          e.message,
         color: "red",
         autoClose: 5000,
       });
-      form.reset();
-    } else {
-      // Get the token out of the URL and attempt API call
-      console.log("The reset password token is", router.query.token);
-      const resetPasswordPayload: ResetPasswordPayload = {
-        token: parseRouterQueryParam(router.query.token),
-        newPassword: form.values.password,
-      };
-      try {
-        await resetPasswordService(resetPasswordPayload);
-        setIsResetSuccessful(true);
-      } catch (e: AxiosError | any) {
-        notifications.show({
-          message:
-            (e.response && e.response.data && e.response.data.message) ||
-            e.message,
-          color: "red",
-          autoClose: 5000,
-        });
-      }
     }
   };
 
