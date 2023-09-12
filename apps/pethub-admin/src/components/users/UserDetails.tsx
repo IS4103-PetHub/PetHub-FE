1cb782731605c7d9a12c41e309f3101123d96175
lokehsienjie <loke.hj@u.nus.edu>
2023-09-13 00:15:20 +0800

    refactor UserDetails code

--- apps/pethub-admin/src/components/users/UserDetails.tsx
+++ apps/pethub-admin/src/components/users/UserDetails.tsx
@@ -6,21 +6,253 @@ import {
   Text,
   Modal,
   Title,
-  Badge,
   Container,
   Grid,
   Col,
+  Center,
+  Box,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useDisclosure } from "@mantine/hooks";
+import AccountStatusBadge from "web-ui/shared/AccountStatusBadge";
 import { formatAccountTypeEnum } from "@/components/util/EnumHelper";
 import { AccountStatusEnum, AccountTypeEnum } from "@/types/constants";
-import { InternalUser, PetBusiness, PetOwner } from "@/types/types";
+import { InternalUser, PetBusiness, PetOwner, User } from "@/types/types";
 type UserDetailsProps = {
   user: PetOwner | PetBusiness | InternalUser | null;
   // onDeactivate: () => void; This method should call hook to deactivate user acc
   // onActivate: () => void; This method should call hook to activate user acc
 };
+
+const getUserName = (user: any): string => {
+  switch (user.accountType) {
+    case AccountTypeEnum.InternalUser:
+    case AccountTypeEnum.PetOwner:
+      return `${user.firstName} ${user.lastName}`;
+    case AccountTypeEnum.PetBusiness:
+      return user.companyName;
+    default:
+      return "";
+  }
+};
+
+const UserDetail = ({ user, userName }: { user: User; userName: string }) => (
+  <>
+    <Paper p="md" shadow="xs" style={{ maxWidth: "600px", margin: "0 auto" }}>
+      <Text align="center" size="xl" weight={500}>
+        {userName}
+      </Text>
+      <Divider my="sm" variant="dashed" />
+
+      <Grid gutter="md">
+        <Col span={6}>
+          <Text>User ID:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.userId}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Email:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.email}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Account Type:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{formatAccountTypeEnum(user.accountType)}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Account Status:</Text>
+        </Col>
+        <Col span={6}>
+          <AccountStatusBadge accountStatus={user.accountStatus} size="lg" />
+        </Col>
+
+        <Col span={6}>
+          <Text>Date Created:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{new Date(user.dateCreated).toLocaleDateString()}</Text>
+        </Col>
+        <Col span={6}>
+          <Text>Date Last Updated:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>
+            {user.lastUpdated
+              ? new Date(user.lastUpdated).toLocaleDateString()
+              : "-"}
+          </Text>
+        </Col>
+      </Grid>
+    </Paper>
+  </>
+);
+
+const ActivateDeactivateButton = ({
+  user,
+  openModal,
+}: {
+  user: User;
+  openModal: () => void;
+}) => (
+  <Grid gutter="md">
+    <Col span={12}>
+      {user.accountStatus === AccountStatusEnum.Active && (
+        <Button
+          fullWidth
+          variant="outline"
+          size="lg"
+          onClick={openModal}
+          color="red"
+        >
+          Deactivate
+        </Button>
+      )}
+      {user.accountStatus === AccountStatusEnum.Inactive && (
+        <Button fullWidth variant="outline" size="lg" color="teal">
+          Activate
+        </Button>
+      )}
+    </Col>
+  </Grid>
+);
+
+const InternalUserDetails = ({
+  user,
+  open,
+  userName,
+}: {
+  user: InternalUser;
+  open: () => void;
+  userName: string;
+}) => (
+  <>
+    <UserDetail user={user} userName={userName} />
+    <Paper p="md" shadow="xs" style={{ maxWidth: "600px", margin: "0 auto" }}>
+      <Grid gutter="md">
+        <Col span={6}>
+          <Text>First Name:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.firstName}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Last Name:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.lastName}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Admin Role:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.adminRole}</Text>
+        </Col>
+      </Grid>
+    </Paper>
+    <ActivateDeactivateButton user={user} openModal={open} />
+  </>
+);
+
+const PetBusinessDetails = ({
+  user,
+  open,
+  userName,
+}: {
+  user: PetBusiness;
+  open: () => void;
+  userName: string;
+}) => (
+  <>
+    <UserDetail user={user} userName={userName} />
+    <Paper p="md" shadow="xs" style={{ maxWidth: "600px", margin: "0 auto" }}>
+      <Grid gutter="md">
+        <Col span={6}>
+          <Text>UEN:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.uen}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Business Type:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.businessType || "-"}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Business Description:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.businessDescription || "-"}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Website:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.websiteURL || "-"}</Text>
+        </Col>
+      </Grid>
+    </Paper>
+    <ActivateDeactivateButton user={user} openModal={open} />
+  </>
+);
+
+const PetOwnerDetails = ({
+  user,
+  open,
+  userName,
+}: {
+  user: PetOwner;
+  open: () => void;
+  userName: string;
+}) => (
+  <>
+    <UserDetail user={user} userName={userName} />
+    <Paper p="md" shadow="xs" style={{ maxWidth: "600px", margin: "0 auto" }}>
+      <Grid gutter="md">
+        <Col span={6}>
+          <Text>First Name:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.firstName}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Last Name:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.lastName}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Date of Birth:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{new Date(user.dateOfBirth).toLocaleDateString()}</Text>
+        </Col>
+
+        <Col span={6}>
+          <Text>Contact Number:</Text>
+        </Col>
+        <Col span={6}>
+          <Text>{user.contactNumber}</Text>
+        </Col>
+      </Grid>
+    </Paper>
+    <ActivateDeactivateButton user={user} openModal={open} />
+  </>
+);
 
 type DeactivateAccountModalProps = {
   closeModal: () => void;
@@ -74,373 +306,51 @@ const UserDetails = ({ user }: UserDetailsProps) => {
   const [opened, { open, close }] = useDisclosure(false);
 
   if (!user) return null;
-  // const userType = user ? user.accountType : null;
-  // const typedUser =
-  //   userType === AccountTypeEnum.InternalUser
-  //     ? (user as InternalUser)
-  //     : userType === AccountTypeEnum.PetBusiness
-  //     ? (user as PetBusiness)
-  //     : userType === AccountTypeEnum.PetOwner
-  //     ? (user as PetOwner)
-  //     : user; //default
 
-  // const displayName =
-  //   userType === AccountTypeEnum.InternalUser ||
-  //   userType === AccountTypeEnum.PetOwner
-  //     ? (typedUser as InternalUser | PetOwner).firstName +
-  //       " " +
-  //       (typedUser as InternalUser | PetOwner).lastName
-  //     : userType === AccountTypeEnum.PetBusiness
-  //     ? (typedUser as PetBusiness).companyName
-  //     : ""; //default
+  const userName = getUserName(user);
+  let UserDetailsComponent = null;
 
   switch (user.accountType) {
     case AccountTypeEnum.InternalUser:
-      const internalUser = user as InternalUser;
-      return (
-        <Paper
-          p="md"
-          shadow="xs"
-          style={{ maxWidth: "600px", margin: "0 auto" }}
-        >
-          <Container size="md">
-            <Text align="center" size="xl" weight={500}>
-              {internalUser.firstName} {internalUser.lastName}
-            </Text>
-            <Divider my="sm" variant="dashed" />
-
-            <Grid gutter="md">
-              <Col span={6}>
-                <Text>User ID:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{internalUser.userId}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>First Name:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{internalUser.firstName}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Last Name:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{internalUser.lastName}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Admin Role:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{internalUser.adminRole}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Email:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{internalUser.email}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Account Type:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{formatAccountTypeEnum(internalUser.accountType)}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Account Status:</Text>
-              </Col>
-              <Col span={6}>
-                <Badge
-                  color={
-                    internalUser.accountStatus === AccountStatusEnum.Active
-                      ? "green"
-                      : "red"
-                  }
-                >
-                  {internalUser.accountStatus}
-                </Badge>
-              </Col>
-
-              <Col span={6}>
-                <Text>Date Created:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>
-                  {new Date(internalUser.dateCreated).toLocaleDateString()}
-                </Text>
-              </Col>
-              <Col span={6}>
-                <Text>Date Last Updated:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>
-                  {internalUser.lastUpdated
-                    ? new Date(internalUser.lastUpdated).toLocaleDateString()
-                    : "-"}
-                </Text>
-              </Col>
-            </Grid>
-
-            <Divider my="md" />
-
-            <Group spacing="md" position="center">
-              <Button onClick={open} color="red">
-                Deactivate
-              </Button>
-              <Button color="teal">Activate</Button>
-            </Group>
-
-            <DeactivateAccountModal
-              closeModal={close}
-              opened={opened}
-              name={internalUser.firstName + " " + internalUser.lastName}
-            />
-          </Container>
-        </Paper>
+      UserDetailsComponent = (
+        <InternalUserDetails
+          user={user as InternalUser}
+          open={open}
+          userName={userName}
+        />
       );
+      break;
     case AccountTypeEnum.PetBusiness:
-      const petBusiness = user as PetBusiness;
-      return (
-        <Paper
-          p="md"
-          shadow="xs"
-          style={{ maxWidth: "600px", margin: "0 auto" }}
-        >
-          <Container size="md">
-            <Text align="center" size="xl" weight={500}>
-              {petBusiness.companyName}
-            </Text>
-            <Divider my="sm" variant="dashed" />
-
-            <Grid gutter="md">
-              <Col span={6}>
-                <Text>User ID:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petBusiness.userId}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>UEN:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petBusiness.uen}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Business Type:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petBusiness.businessType || "-"}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Business Description:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petBusiness.businessDescription || "-"}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Website:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petBusiness.websiteURL || "-"}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Email:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petBusiness.email}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Account Type:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{formatAccountTypeEnum(petBusiness.accountType)}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Account Status:</Text>
-              </Col>
-              <Col span={6}>
-                <Badge
-                  color={
-                    petBusiness.accountStatus === AccountStatusEnum.Active
-                      ? "green"
-                      : "red"
-                  }
-                >
-                  {petBusiness.accountStatus}
-                </Badge>
-              </Col>
-
-              <Col span={6}>
-                <Text>Date Created:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>
-                  {new Date(petBusiness.dateCreated).toLocaleDateString()}
-                </Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Date Last Updated:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>
-                  {petBusiness.lastUpdated
-                    ? new Date(petBusiness.lastUpdated).toLocaleDateString()
-                    : "-"}
-                </Text>
-              </Col>
-            </Grid>
-
-            <Divider my="md" />
-
-            <Group spacing="md" position="center">
-              <Button onClick={open} color="red">
-                Deactivate
-              </Button>
-              <Button color="teal">Activate</Button>
-            </Group>
-
-            <DeactivateAccountModal
-              closeModal={close}
-              opened={opened}
-              name={petBusiness.companyName}
-            />
-          </Container>
-        </Paper>
+      UserDetailsComponent = (
+        <PetBusinessDetails
+          user={user as PetBusiness}
+          open={open}
+          userName={userName}
+        />
       );
+      break;
     case AccountTypeEnum.PetOwner:
-      const petOwner = user as PetOwner;
-      return (
-        <Paper
-          p="md"
-          shadow="xs"
-          style={{ maxWidth: "600px", margin: "0 auto" }}
-        >
-          <Container size="md">
-            <Text align="center" size="xl" weight={500}>
-              {petOwner.firstName} {petOwner.lastName}
-            </Text>
-            <Divider my="sm" variant="dashed" />
-
-            <Grid gutter="md">
-              <Col span={6}>
-                <Text>User ID:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petOwner.userId}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>First Name:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petOwner.firstName}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Last Name:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petOwner.lastName}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Date of Birth:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>
-                  {new Date(petOwner.dateOfBirth).toLocaleDateString()}
-                </Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Contact Number:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petOwner.contactNumber}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Email:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{petOwner.email}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Account Type:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>{formatAccountTypeEnum(petOwner.accountType)}</Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Account Status:</Text>
-              </Col>
-              <Col span={6}>
-                <Badge
-                  color={
-                    petOwner.accountStatus === AccountStatusEnum.Active
-                      ? "green"
-                      : "red"
-                  }
-                >
-                  {petOwner.accountStatus}
-                </Badge>
-              </Col>
-
-              <Col span={6}>
-                <Text>Date Created:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>
-                  {new Date(petOwner.dateCreated).toLocaleDateString()}
-                </Text>
-              </Col>
-
-              <Col span={6}>
-                <Text>Date Last Updated:</Text>
-              </Col>
-              <Col span={6}>
-                <Text>
-                  {petOwner.lastUpdated
-                    ? new Date(petOwner.lastUpdated).toLocaleDateString()
-                    : "-"}
-                </Text>
-              </Col>
-            </Grid>
-
-            <Divider my="md" />
-
-            <Group spacing="md" position="center">
-              <Button onClick={open} color="red">
-                Deactivate
-              </Button>
-              <Button color="teal">Activate</Button>
-            </Group>
-
-            <DeactivateAccountModal
-              closeModal={close}
-              opened={opened}
-              name={petOwner.firstName + " " + petOwner.lastName}
-            />
-          </Container>
-        </Paper>
+      UserDetailsComponent = (
+        <PetOwnerDetails
+          user={user as PetOwner}
+          open={open}
+          userName={userName}
+        />
       );
+      break;
+    default:
+      return null;
   }
+  return (
+    <>
+      {UserDetailsComponent}
+      <DeactivateAccountModal
+        closeModal={close}
+        opened={opened}
+        name={userName}
+      />
+    </>
+  );
 };
 
 export default UserDetails;
