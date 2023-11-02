a6cfd800be6e1e1d04b222cc47c75102cbff0861
Ryo <ryo200013@gmail.com>
2023-11-02 23:11:58 +0800

    revs

--- apps/pethub-main/src/components/service-listing-management/ReviewStatisticsGroup.tsx
+++ apps/pethub-main/src/components/service-listing-management/ReviewStatisticsGroup.tsx
@@ -37,13 +37,13 @@ const ReviewStatisticsGroup = ({
   serviceListing,
 }: ReviewStatisticsGroupProps) => {
   const linedata1 = [
-    ["Month", "1 Paw", "2 Paw", "3 Paw", "4 Paw", "5 Paw"],
-    ["May", 20, 15, 48, 65, 130],
-    ["Jun", 30, 20, 45, 80, 100],
-    ["Jul", 35, 10, 50, 90, 150],
-    ["Aug", 40, 15, 60, 100, 120],
-    ["Sep", 45, 20, 70, 105, 140],
-    ["Oct", 50, 25, 80, 110, 260],
+    ["Month", "5 Paw", "4 Paw", "3 Paw", "2 Paw", "1 Paw"],
+    ["May", 130, 65, 48, 15, 20],
+    ["Jun", 100, 80, 45, 20, 30],
+    ["Jul", 150, 90, 50, 10, 35],
+    ["Aug", 120, 100, 60, 15, 40],
+    ["Sep", 140, 105, 70, 20, 45],
+    ["Oct", 260, 110, 80, 25, 50],
   ];
 
   const lineoptions1 = {
@@ -58,7 +58,7 @@ const ReviewStatisticsGroup = ({
     legend: { position: "bottom" },
     animation: {
       startup: true,
-      easing: "inAndOut",
+      easing: "out",
       duration: 3000,
     },
   };
@@ -85,31 +85,61 @@ const ReviewStatisticsGroup = ({
     legend: { position: "bottom" },
     animation: {
       startup: true,
-      easing: "linear",
+      easing: "out",
+      duration: 3000,
+    },
+  };
+
+  const piedata = [
+    ["Rating", "Count"],
+    ["5 Paw (900)", 900],
+    ["4 Paw (550)", 550],
+    ["3 Paw (353)", 353],
+    ["2 Paw (105)", 105],
+    ["1 Paw (220)", 220],
+  ];
+
+  const pieoptions = {
+    title: "Ratings Distribution (past 6 months)",
+    is3D: true,
+    animation: {
+      startup: true,
+      easing: "out",
       duration: 3000,
     },
   };
 
   return (
     <Box mb="xs">
-      <Box mb="xs">
-        <Chart
-          chartType="LineChart"
-          width="100%"
-          height="400px"
-          data={linedata1}
-          options={lineoptions1}
-        />
-      </Box>
-      <Box mb="xs">
-        <Chart
-          chartType="LineChart"
-          width="100%"
-          height="400px"
-          data={linedata2}
-          options={lineoptions2}
-        />
-      </Box>
+      <Grid columns={12}>
+        <Grid.Col span={12}>
+          <Chart
+            chartType="LineChart"
+            width="100%"
+            height="400px"
+            data={linedata1}
+            options={lineoptions1}
+          />
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Chart
+            chartType="LineChart"
+            width="100%"
+            height="400px"
+            data={linedata2}
+            options={lineoptions2}
+          />
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Chart
+            chartType="PieChart"
+            data={piedata}
+            options={pieoptions}
+            width={"100%"}
+            height={"400px"}
+          />
+        </Grid.Col>
+      </Grid>
     </Box>
   );
 };
