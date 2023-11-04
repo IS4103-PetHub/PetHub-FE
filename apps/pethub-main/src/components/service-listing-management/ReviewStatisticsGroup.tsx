b4c5a563c0999dbc1973b016a219420b85ca6a44
Ryo <ryo200013@gmail.com>
2023-11-04 23:31:13 +0800

    review charts

--- apps/pethub-main/src/components/service-listing-management/ReviewStatisticsGroup.tsx
+++ apps/pethub-main/src/components/service-listing-management/ReviewStatisticsGroup.tsx
@@ -26,59 +26,52 @@ import { IconClockHour8 } from "@tabler/icons-react";
 import { useRouter } from "next/router";
 import React, { useEffect, useRef, useState } from "react";
 import Chart from "react-google-charts";
-import { Review, ServiceListing } from "shared-utils";
+import { Review, ReviewStatsResponse, ServiceListing } from "shared-utils";
 import StarRating from "../review/StarRating";
 
 interface ReviewStatisticsGroupProps {
-  serviceListing: ServiceListing;
+  reviewStats: ReviewStatsResponse;
 }
 
-const ReviewStatisticsGroup = ({
-  serviceListing,
-}: ReviewStatisticsGroupProps) => {
-  const linedata1 = [
-    ["Month", "5 Paw", "4 Paw", "3 Paw", "2 Paw", "1 Paw"],
-    ["May", 130, 65, 48, 15, 20],
-    ["Jun", 100, 80, 45, 20, 30],
-    ["Jul", 150, 90, 50, 10, 35],
-    ["Aug", 120, 100, 60, 15, 40],
-    ["Sep", 140, 105, 70, 20, 45],
-    ["Oct", 260, 110, 80, 25, 50],
+const ReviewStatisticsGroup = ({ reviewStats }: ReviewStatisticsGroupProps) => {
+  const theme = useMantineTheme();
+
+  const themeColors = [
+    theme.colors.red[7], // 5 paw
+    theme.colors.orange[7],
+    theme.colors.green[7],
+    theme.colors.indigo[5],
+    theme.colors.grape[7], // 1 paw
   ];
 
-  const lineoptions1 = {
+  const ratingCountDistributionOptions = {
     hAxis: {
       title: "Time",
     },
     vAxis: {
       title: "Number of ratings",
+      minValue: 0,
+      format: "0", // integer
     },
     title: "Ratings / Time (past 6 months)",
-    curveType: "function",
+    curveType: "none",
     legend: { position: "bottom" },
     animation: {
       startup: true,
       easing: "out",
       duration: 3000,
     },
+    colors: themeColors,
   };
 
-  const linedata2 = [
-    ["Month", "Average rating"],
-    ["May", 3.5],
-    ["Jun", 3.7],
-    ["Jul", 4.3],
-    ["Aug", 4.2],
-    ["Sep", 4.9],
-    ["Oct", 4.8],
-  ];
-
-  const lineoptions2 = {
+  const averageRatingOptions = {
     hAxis: {
       title: "Time",
     },
     vAxis: {
       title: "Average Rating",
+      ticks: [1, 2, 3, 4, 5], // brackets
+      minValue: 1,
     },
     title: "Average Rating / Time (past 6 months)",
     curveType: "function",
@@ -90,16 +83,7 @@ const ReviewStatisticsGroup = ({
     },
   };
 
-  const piedata = [
-    ["Rating", "Count"],
-    ["5 Paw (900)", 900],
-    ["4 Paw (550)", 550],
-    ["3 Paw (353)", 353],
-    ["2 Paw (105)", 105],
-    ["1 Paw (220)", 220],
-  ];
-
-  const pieoptions = {
+  const ratingCountOptions = {
     title: "Ratings Distribution (past 6 months)",
     is3D: true,
     animation: {
@@ -107,6 +91,7 @@ const ReviewStatisticsGroup = ({
       easing: "out",
       duration: 3000,
     },
+    colors: themeColors,
   };
 
   return (
@@ -117,8 +102,8 @@ const ReviewStatisticsGroup = ({
             chartType="LineChart"
             width="100%"
             height="400px"
-            data={linedata1}
-            options={lineoptions1}
+            data={reviewStats?.ratingCountDistributionData}
+            options={ratingCountDistributionOptions}
           />
         </Grid.Col>
         <Grid.Col span={6}>
@@ -126,15 +111,15 @@ const ReviewStatisticsGroup = ({
             chartType="LineChart"
             width="100%"
             height="400px"
-            data={linedata2}
-            options={lineoptions2}
+            data={reviewStats?.averageReviewData}
+            options={averageRatingOptions}
           />
         </Grid.Col>
         <Grid.Col span={6}>
           <Chart
             chartType="PieChart"
-            data={piedata}
-            options={pieoptions}
+            data={reviewStats?.ratingCountData}
+            options={ratingCountOptions}
             width={"100%"}
             height={"400px"}
           />
