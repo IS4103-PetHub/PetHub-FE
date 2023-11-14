becc28a6d98330216b93c397e8a35274c6ac2531
Ryo <ryo200013@gmail.com>
2023-11-15 03:50:57 +0800

    tele share button

--- packages/web-ui/shared/article/ArticleSharePopover.tsx
+++ packages/web-ui/shared/article/ArticleSharePopover.tsx
@@ -14,6 +14,7 @@ import { notifications } from "@mantine/notifications";
 import {
   IconBrandFacebook,
   IconBrandLinkedin,
+  IconBrandTelegram,
   IconBrandTwitter,
   IconCopy,
   IconLink,
@@ -33,6 +34,11 @@ const ArticleSharePopover = ({}: ArticleSharePopoverProps) => {
   const [sharePopoverOpen, toggleSharePopoverOpen] = useToggle();
 
   const currentUrl = `${window?.location?.origin}${router.asPath}`;
+
+  const SHARE_URL_LINKEDIN = `https://www.linkedin.com/sharing/share-offsite/?url=`;
+  const SHARE_URL_FACEBOOK = `https://www.facebook.com/sharer/sharer.php?u=`;
+  const SHARE_URL_TWITTER = `https://twitter.com/intent/tweet?url=`;
+  const SHARE_URL_TELEGRAM = `https://telegram.me/share/url?url=`;
 
   // This dummy article link is used to replace all sharing instances for this application. This is because the application is not hosted on the public web yet.
   const DUMMY_MEDIUM_URL = `https://medium.com/@techcontentspecialist/the-abandoned-pet-1b15645cab5d`;
@@ -62,24 +68,23 @@ const ArticleSharePopover = ({}: ArticleSharePopoverProps) => {
   };
 
   const shareOnLinkedIn = () => {
-    const url = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(
-      DUMMY_MEDIUM_URL,
-    )}`;
-    openUrlInNewWindow(url, "share-to-linkedIn");
+    const url = SHARE_URL_LINKEDIN + encodeURIComponent(DUMMY_MEDIUM_URL);
+    openUrlInNewWindow(url, "linkedin-share-dialog");
   };
 
   const shareOnFacebook = () => {
-    const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-      DUMMY_MEDIUM_URL,
-    )}`;
-    window.open(url, "linkedin-share-dialog", "width=800,height=600");
+    const url = SHARE_URL_FACEBOOK + encodeURIComponent(DUMMY_MEDIUM_URL);
+    openUrlInNewWindow(url, "facebook-share-dialog");
   };
 
   const shareOnTwitter = () => {
-    const url = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
-      DUMMY_MEDIUM_URL,
-    )}`;
-    window.open(url, "linkedin-share-dialog", "width=800,height=600");
+    const url = SHARE_URL_TWITTER + encodeURIComponent(DUMMY_MEDIUM_URL);
+    openUrlInNewWindow(url, "twitter-share-dialog");
+  };
+
+  const shareOnTelegram = () => {
+    const url = SHARE_URL_TELEGRAM + encodeURIComponent(currentUrl);
+    openUrlInNewWindow(url, "telegram-share-dialog");
   };
 
   return (
@@ -126,19 +131,27 @@ const ArticleSharePopover = ({}: ArticleSharePopoverProps) => {
         >
           Share on LinkedIn
         </Button>
-        <Button
+        {/* I don't have facebook so can't test this 🗿 */}
+        {/* <Button
           {...LINK_BUTTON_PROPS}
           leftIcon={<IconBrandFacebook size="1.25rem" />}
           onClick={shareOnFacebook}
         >
           Share on Facebook
-        </Button>
+        </Button> */}
         <Button
           {...LINK_BUTTON_PROPS}
           leftIcon={<IconBrandTwitter size="1.25rem" />}
           onClick={shareOnTwitter}
         >
           Share on Twitter
+        </Button>
+        <Button
+          {...LINK_BUTTON_PROPS}
+          leftIcon={<IconBrandTelegram size="1.25rem" />}
+          onClick={shareOnTelegram}
+        >
+          Share on Telegram
         </Button>
       </Popover.Dropdown>
     </Popover>
